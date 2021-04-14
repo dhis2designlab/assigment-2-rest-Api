f85679ab7811cc2a7efb4c325620396e36e27bc5
sofiegwendolyn <gwendob@student.matnat.uio.no>
2021-04-14 14:32:15 +0200

    add encoding

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Assigment-2-rest-Api
+
+Test
+--http://127.0.0.1:5000/api?year=2018,2019&country=Afghanistan,Norway&page=1
+
+--http://127.0.0.1:5000/api/fact?year=2018,2019&country=Norway,Afghanistan&page=1&limit=4&sort=year,value&order=desc
--- server.py
+++ server.py
@@ -6,10 +6,12 @@ import requests
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
+keyParam = ["page", "sort", "limit", "order"] # pagination, sort, filtering
+
 if __name__ == '__main__':
     SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
     json_url = os.path.join(SITE_ROOT, "static/data", "who.json")
-    data = json.load(open(json_url)) # data
+    data = json.load(open(json_url, encoding='utf-8')) # data
 
 @app.route('/', methods=['GET'])
 def home():
@@ -21,22 +23,18 @@ def home():
 def getDimension():
     query_parameters = request.args
     curr_data = data["dimension"]
-    page = query_parameters.get('page', type = int)
     response = curr_data
     
     args = [i for i in query_parameters.keys()]
     for arg in args:
-        if arg != "page":
+        if arg not in keyParam:
             param = query_parameters.get(arg) # get value given with the arg
-            response = [c for c in curr_data if c["dims"][arg.upper()] in param] # get all data that matches
+            response = [c for c in curr_data if c[arg] in param] # get all data that matches
             curr_data = response # update data
 
-            # Paginating – For example, ?page=2, ?p=2 or viewItems=10-30
-    if page:
-        element_per_page = 10
-        response = list(response)[element_per_page * (page - 1): ] # skip elements based on page provided
-    response = jsonify(response)
-
+    # Paginating – For example, ?page=2, ?p=2 or viewItems=10-30
+    response = applyAllKeyParam(query_parameters, response) # apply advanced filtering, sorting and pagiantion
+    response = jsonify(response) 
     # Enable Access-Control-Allow-Origin
     response.headers.add("Access-Control-Allow-Origin", "*")
     
@@ -49,21 +47,18 @@ def getFact():
     query_parameters = request.args
     curr_data = data["fact"]
     response = curr_data
-    page = query_parameters.get('page', type = int)
 
     args = [i for i in query_parameters.keys()]
     print(args)
     for arg in args:
-        if arg != "page":
+        if arg not in keyParam:
             param = query_parameters.get(arg) # get value given with the arg
             response = [c for c in curr_data if c["dims"][arg.upper()] in param] # get all data that matches
             curr_data = response # update data
 
-            # Paginating – For example, ?page=2, ?p=2 or viewItems=10-30
-    if page:
-        element_per_page = 10
-        response = list(response)[element_per_page * (page - 1): ] # skip elements based on page provided
-    response = jsonify(response)
+    # Paginating – For example, ?page=2, ?p=2 or viewItems=10-30
+    response = applyAllKeyParam(query_parameters, response) # apply advanced filtering, sorting and pagiantion
+    response = jsonify(response) 
 
     # Enable Access-Control-Allow-Origin
     response.headers.add("Access-Control-Allow-Origin", "*")
@@ -75,24 +70,58 @@ def getAPI():
     query_parameters = request.args
     curr_data = data["fact"]
     response = data
-    page = query_parameters.get('page', type = int)
+    
 
     args = [i for i in query_parameters.keys()]
     for arg in args:
-        if arg != "page":
+        if arg not in keyParam:
             param = query_parameters.get(arg) # get value given with the arg
             response = [c for c in curr_data if c["dims"][arg.upper()] in param] # get all data that matches
             curr_data = response # update data
+            # filtering numbers? e.g ?value=20-50
 
-            # Paginating – For example, ?page=2, ?p=2 or viewItems=10-30
-    if page:
-        element_per_page = 10
-        response = list(response)[element_per_page * (page - 1): ] # skip elements based on page provided
-    response = jsonify(response)
+    # Paginating – For example, ?page=2, ?p=2 or viewItems=10-30
+    response = applyAllKeyParam(query_parameters, response) # apply advanced filtering, sorting and pagiantion
+    response = jsonify(response) 
 
     # Enable Access-Control-Allow-Origin
     response.headers.add("Access-Control-Allow-Origin", "*")
     
     return response, 200
+
+
+# Filtering, sorting, pagination
+def applyAllKeyParam(query_parameters, response):
+    limit = query_parameters.get('limit', type = int)
+    page = query_parameters.get('page', type = int)
+    sort = query_parameters.get('sort')
+    order = query_parameters.get('order', default="asc")
+    filtering = query_parameters.get('filtering', type = int)
+    
+    
+    isDescending = False if order == "asc" else True
+    if sort:
+        bits = sort.split(",")
+        response = sorted(response, key=lambda x : ([x["dims"][key.upper()] for key in bits if key.upper() != "VALUE"]), reverse=isDescending)
+        if "value" in bits:
+            response = sorted(response, key=lambda x : x["VALUE"], reverse=isDescending)
+
+    if page:
+        element_per_page = 10
+        if limit:
+            element_per_page = limit
+        
+        start = element_per_page * (page - 1) # start index
+        end = start + element_per_page # end index
+
+        if page == 1: # if its the first page
+            end = element_per_page # define end as 10 
+        response = list(response)[start: end] # skip elements based on page provided
+
+    elif limit:
+        response = list(response)[:limit] # override value
+
+    return response
+    
 
 app.run()
